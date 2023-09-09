9261b1fcf0ec78bf032835dd1a54073e99db67d1
maxvelkir <kiriakovmaxim@gmail.com>
2023-09-09 15:23:35 -0700

    remove unwanted code from imagine-ws.ts

--- example/imagine-ws.ts
+++ example/imagine-ws.ts
@@ -28,40 +28,6 @@ async function main() {
   if (!Imagine) {
     return;
   }
-  const reroll = await client.Reroll({
-    msgId: <string>Imagine.id,
-    hash: <string>Imagine.hash,
-    flags: Imagine.flags,
-    loading: (uri: string, progress: string) => {
-      console.log("Reroll.loading", uri, "progress", progress);
-    },
-  });
-  console.log({ reroll });
-
-  const Variation = await client.Variation({
-    index: 2,
-    msgId: <string>Imagine.id,
-    hash: <string>Imagine.hash,
-    flags: Imagine.flags,
-    loading: (uri: string, progress: string) => {
-      console.log("Variation.loading", uri, "progress", progress);
-    },
-  });
-
-  console.log({ Variation });
-  if (!Variation) {
-    return;
-  }
-  const Upscale = await client.Upscale({
-    index: 2,
-    msgId: <string>Variation.id,
-    hash: <string>Variation.hash,
-    flags: Variation.flags,
-    loading: (uri: string, progress: string) => {
-      console.log("Upscale.loading", uri, "progress", progress);
-    },
-  });
-  console.log({ Upscale });
 
   client.Close();
 }
